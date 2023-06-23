117d37e37ca0bd290a657d6caf6f1bb9452b7283
julian-21 <111974152+mufidbrown@users.noreply.github.com>
2023-06-24 01:06:33 +0700

    add migration table

---
+++ database/migrations/2023_06_19_173024_create_detail_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detail_user', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('type_user_id');
+            $table->string('contact')->nullable();
+            $table->longText('address')->nullable();
+            $table->longText('photo')->nullable();
+            $table->enum('gender', [1,2])->nullable();
+            $table->timestamps();
+            $table->softDeletes(); //softdeletes wajib ada, sewaktu2 bikin modul yang diperuntukan untuk sebuah aturan table yang data nya tidak boleh di hapus, dan didalam project besar softdeletes wajib ada. dan didalam sofdeletes ini tidak perlu mengubah pengaturan migrations sewaktu2, karena sudah otomatis ada softdiletes.
+
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detail_user');
+    }
+};
